8c41a72cfacdb1191a971ec46f8aeb2e8398f56e
aumeisters <54903608+aumeisters@users.noreply.github.com>
2019-09-08 15:34:16 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let fDeg = 32;
+let roubles = 25000;
+let a = 8;
+let b = 45;
+let c = -18;
+
+cDeg = (fDeg * 9/5) + 32;
+dollars = roubles * 0.015;
+x1 = (-b + (Math.sqrt((b * b) - 4 * a * c))) / (2 * a);
+x2 = (-b - (Math.sqrt((b * b) - 4 * a * c))) / (2 * a);
+
+console.log(cDeg, dollars, x1, x2);
